cba30388ed6c2f87544ac90f96e6eecc6936ec81
Samuel Yallop <samuel.yallop@pusher.com>
2019-04-11 21:43:46 +0100

    Hardcode tls on listener

--- pilot/pkg/networking/plugin/h2sidecar/h2sidecar.go
+++ pilot/pkg/networking/plugin/h2sidecar/h2sidecar.go
@@ -17,7 +17,9 @@ package h2sidecar
 import (
 	xdsapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 
+	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking/plugin"
 	"istio.io/istio/pilot/pkg/networking/util"
@@ -42,7 +44,78 @@ func (Plugin) OnOutboundListener(in *plugin.InputParams, mutable *plugin.Mutable
 // Can be used to add additional filters (e.g., mixer filter) or add more stuff to the HTTP connection manager
 // on the inbound path
 func (Plugin) OnInboundListener(in *plugin.InputParams, mutable *plugin.MutableObjects) error {
+	if in.Node == nil {
+		return nil
+	}
+
+	if in.Node.Type != model.SidecarProxy {
+		return nil
+	}
+
+	if in.ListenerCategory != networking.EnvoyFilter_ListenerMatch_SIDECAR_INBOUND {
+		return nil
+	}
+
+	if in.ServiceInstance == nil {
+		return nil
+	}
+
+	if mutable == nil {
+		return nil
+	}
+
+	if in.Port.Port != 10443 {
+		return nil
+	}
+
+	if in.Port.Name != "http2-elements" && in.Port.Name != "https-elements" {
+		return nil
+	}
+	log.Infof("h2sidecar: Manipulating inbound listener for port %v %v bind %v and initial state %v", in.Port, in.Port.Name, in.Bind, mutable)
+	setListenerH2S(mutable)
+	log.Infof("h2sidecar: %v %v %v mutated state: %v", in.Port, in.Port.Name, in.Bind, mutable)
+
 	return nil
+}
+
+// setListenerH2S configures an listener to talk H2S.
+// TLC certs are hardcoded and ALPN is H2 only.
+func setListenerH2S(mutable *plugin.MutableObjects) {
+	if len(mutable.FilterChains) < 2 {
+		log.Info("h2sidecar: Expected at least two listeners in filterchain (mtls and then the default)")
+		return
+	}
+
+	for ix, filterChain := range mutable.FilterChains {
+		log.Infof("h2sidecar: filterchain index %v is %v", ix, filterChain)
+	}
+
+	// 0 index is expected to be the mtlschain
+	filterChain := mutable.FilterChains[1]
+	log.Infof("h2sidecar: Mutating listener filterChain %v", filterChain)
+
+	if filterChain.TLSContext == nil {
+		log.Infof("h2sidecar: TLSContext was nil")
+		filterChain.TLSContext = &auth.DownstreamTlsContext{}
+	}
+
+	if filterChain.TLSContext.CommonTlsContext == nil {
+		log.Infof("h2sidecar: CommonTLSContext was nil")
+		filterChain.TLSContext.CommonTlsContext = &auth.CommonTlsContext{}
+	}
+
+	// TODO: Should be configured to value set in destinationrule rather than hardcoded.
+	filterChain.TLSContext.CommonTlsContext.TlsCertificates = []*auth.TlsCertificate{
+		{CertificateChain: &core.DataSource{Specifier: &core.DataSource_Filename{Filename: "/certs/tls.crt"}},
+			PrivateKey: &core.DataSource{Specifier: &core.DataSource_Filename{Filename: "/certs/tls.key"}},
+		},
+	}
+
+	// Only accept h2 on this listener.
+	filterChain.TLSContext.CommonTlsContext.AlpnProtocols = util.ALPNH2Only
+
+	log.Infof("h2sidecar: mutated filterchain to %v", filterChain)
+	mutable.FilterChains[1] = filterChain
 }
 
 // OnInboundCluster implements the Plugin interface method.
@@ -52,7 +125,6 @@ func (Plugin) OnInboundCluster(in *plugin.InputParams, cluster *xdsapi.Cluster) 
 	}
 
 	if in.Node.Type != model.SidecarProxy {
-		// Only care about sidecar.
 		return
 	}
 
@@ -67,18 +139,17 @@ func (Plugin) OnInboundCluster(in *plugin.InputParams, cluster *xdsapi.Cluster) 
 	if in.Port.Port != 10443 {
 		return
 	}
-	log.Infof("%v", in.Port)
 
-	if in.Port.Name != "http2-elements" || in.Port.Name != "http2-elements" {
+	if in.Port.Name != "http2-elements" && in.Port.Name != "https-elements" {
 		return
 	}
 
-	setALPNH2(cluster)
-	log.Infof("Writing elements cluster: %v", cluster)
+	setClusterALPNH2(cluster)
+	log.Infof("h2sidecar: Writing elements cluster: %v", cluster)
 }
 
-// setALPNH2 sets ALPN protocols to "H2" in the clusters tls context.
-func setALPNH2(cluster *xdsapi.Cluster) {
+// setClusterALPNH2 sets ALPN protocols to "H2" in the clusters tls context.
+func setClusterALPNH2(cluster *xdsapi.Cluster) {
 	if cluster.TlsContext == nil {
 		cluster.TlsContext = &auth.UpstreamTlsContext{}
 	}
