a2a357af358c063af96316c7e0b315f93fc66f04
Ben Pope <ben.pope@pusher.com>
2019-07-24 15:58:22 +0100

    Improve create.sh

--- create.sh
+++ create.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 SHA=$(git rev-parse HEAD)
+OUT_DIR=${1:-../istio-release-$SHA}
 
-helm template install/kubernetes/helm/istio --name istio --namespace istio-system --output-dir ../istio-release-1.1/
-mv ../istio-release-1.1/istio/templates/configmap.yaml ../istio-release-1.1/istio/charts/
-cat install/kubernetes/helm/istio-init/files/crd-* > ../istio-release-1.1/istio/charts/crds.yaml
-for i in ../istio-release-1.1/istio/charts/*/templates/*.yaml; do mv $i $(echo $i | sed -e 's|templates/||g'); done
-for i in ../istio-release-1.1/istio/charts/*/templates; do rmdir $i; done
-for i in ../istio-release-1.1/istio/charts/*/clusterrolebindings.yaml; do mv $i $(echo $i | sed -e 's|clusterrolebindings|crb|g'); done
-for i in ../istio-release-1.1/istio/charts/*/clusterrolebinding.yaml; do mv $i $(echo $i | sed -e 's|clusterrolebinding|crb|g'); done
-for i in ../istio-release-1.1/istio/charts/*/autoscale.yaml; do mv $i $(echo $i | sed -e 's|autoscale|hpa|g'); done
-for i in ../istio-release-1.1/istio/charts/*/deployment.yaml; do sed -i 's|gcr.io/istio-release/\(.*\):release-1.1-latest-daily|quay.io/pusher/istio-\1:'$SHA'|' $i; done
-for i in ../istio-release-1.1/istio/charts/*/deployment.yaml; do sed -i 's|IfNotPresent|Always|' $i; done
+mkdir -p "$OUT_DIR"
+helm template install/kubernetes/helm/istio --name istio --namespace istio-system --output-dir "$OUT_DIR"
+mv "$OUT_DIR"/istio/templates/configmap.yaml "$OUT_DIR"/istio/charts/
+cat install/kubernetes/helm/istio-init/files/crd-* > "$OUT_DIR"/istio/charts/crds.yaml
+for i in "$OUT_DIR"/istio/charts/*/templates/*.yaml; do mv "$i" "${i//templates/}"; done
+for i in "$OUT_DIR"/istio/charts/*/templates; do rmdir "$i"; done
+for i in "$OUT_DIR"/istio/charts/*/clusterrolebinding.yaml; do mv "$i" "${i//clusterrolebinding/crb}"; done
+for i in "$OUT_DIR"/istio/charts/*/autoscale.yaml; do mv "$i" "${i//autoscale/hpa}"; done
+for i in "$OUT_DIR"/istio/charts/*/deployment.yaml; do sed -i 's|gcr.io/istio-release/\(.*\):release-1.2-latest-daily|quay.io/pusher/istio-\1:'"$SHA"'|' "$i"; done
+for i in "$OUT_DIR"/istio/charts/*/deployment.yaml; do sed -i 's|IfNotPresent|Always|' "$i"; done
